65838e478c926e68ab877c0925ca099e5a951e2b
Maria Beatriz Silva <67334485+mariabeatrizsilva@users.noreply.github.com>
2024-02-25 12:44:19 -0700

    Add files via upload

---
+++ murano/Colorcomputer.java
@@ -0,0 +1,13 @@
+class Colorcomputer{
+
+    static void c(float x, float y, float z){
+        System.out.println("vec3(" + x/255 + ", " + y/255+ ", " + z/255+ ");");
+
+    }
+
+
+    public static void main(String[] args){
+         c(185, 255, 71);
+    }
+
+} 
---
+++ murano/README.md
@@ -0,0 +1,1 @@
+Welcome to my Murano Glass Cup renderer! 
